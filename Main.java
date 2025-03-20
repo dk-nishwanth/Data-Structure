2cf860ea5a1f1bb02dedc2cf738f8d39ed2b6ee2
dk-nishwanth <dknishwanth1718@gmail.com>
2025-03-20 14:11:53 +0530

    array -insertion

--- Main.java
+++ Main.java
@@ -1,44 +1,25 @@
-import java.util.Scanner ;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
 
-        System.out.println("Enter the size of the array");
-        int n = scn.nextInt();
-
-        int[] arr = new int[n] ; 
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = scn.nextInt();
-        }
-
-        System.out.println("Enter the element to be inserted at the beginning");
-        
-        int firstElement = scn.nextInt();
-
-
-        System.out.println("Enter the element to be inserted at the end");
-        int lastElement = scn.nextInt();
-
-        // Create a new array with size increased by 2
-        int newSize = arr.length + 2;
-        int[] newArr = new int[newSize];
-
-        // Insert at the beginning
-        newArr[0] = firstElement;
-
-        // Copy old array to new array
-        for (int i = 0; i < arr.length; i++) {
-            newArr[i + 1] = arr[i]; // Shift right by 1
-        }
-
-        // Insert at the end
-        newArr[newArr.length - 1] = lastElement;
-
-        // Print updated array
-        System.out.print("Updated Array: ");
-        for (int i = 0; i < newArr.length; i++) {
-            System.out.print(newArr[i] + " ");
-        }
-    }
+import java.util.Scanner ; 
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		// Getting the number of arrray elements from the user
+		int n = scn.nextInt();
+		
+		
+		// Initialising the array for the given number of elements 
+		int[] arr = new int[n];
+		
+		// Getting array elements from the user 
+		for(int i = 0; i < n; i++){
+		    arr[i] = scn.nextInt();
+		}
+		
+		// Printing the array elements 
+		for(int i = 0; i < n; i++){
+		    System.out.println(arr[i]);
+		}
+	}
 }
