b0a5d123e98924891069c2f024ec7c3f639d693d
dk-nishwanth <dknishwanth1718@gmail.com>
2025-03-20 14:43:07 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,20 +4,11 @@ public class Main
 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		
-		// Getting the number of arrray elements from the user
 		int n = scn.nextInt();
-		
-		
-		// Initialising the array for the given number of elements 
 		int[] arr = new int[n];
-		
-		// Getting array elements from the user 
 		for(int i = 0; i < n; i++){
 		    arr[i] = scn.nextInt();
 		}
-		
-		// Printing the array elements 
 		for(int i = 0; i < n; i++){
 		    System.out.println(arr[i]);
 		}
