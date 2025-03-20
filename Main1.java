fd8089a98dd6329fddcc685d878791729a99a3fb
dk-nishwanth <dknishwanth1718@gmail.com>
2025-03-20 15:44:04 +0530

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -4,14 +4,16 @@ void main()
     int n;
     scanf("%d",&n);
     int arr[n+1];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
+    {
         scanf("%d",&arr[i]);
     }
     int new_element;
     scanf("%d",&new_element);
-    arr[0]=new_element;
-    n--;
-    for(int i=0;i<n;i++){
+    arr[n]=new_element;
+    n++;
+    for(int i=0;i<n;i++)
+    {
         printf("%d",arr[i]);
-    }
+    }
 }
